21fe166433ecb4e66ac02d16b4b078ac79eb0d52
katravathjagadeeshkumar <90197564+katravathjagadeeshkumar@users.noreply.github.com>
2022-04-26 13:15:24 +0530

    Add files via upload

---
+++ Angular Assignment-1/Array Objects/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+	users = [{name:'Jagadeesh',age:25},{name:'Pavan',age:24},{name:'Basha',age:25}];
+
+	
+ }
